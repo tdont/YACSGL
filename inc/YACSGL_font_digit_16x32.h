3218c05bd1c0447f48db945c6450b1652c62ef32
Thibaut DONTAIL <thibaut.dontail@viveris.fr>
2024-02-10 23:45:19 +0100

    [Src]: Adding font for digit only in 16x32 (WIP)

---
+++ inc/YACSGL_font_digit_16x32.h
@@ -0,0 +1,80 @@
+/******************************************************************************
+* Copyright(c) 2022 Thibaut DONTAIL
+*
+* This file is part of YACSGL.
+*
+* YACSGL is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* YACSGL is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with YACSGL.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+/******************** FILE HEADER ********************************************/
+/**\addtogroup ModuleName_External_Documentation
+ * \{ */
+/**\addtogroup SubModuleName
+ * \{ */
+
+/**
+ * \file YAGSGL_font_digit_16x32.h
+ *
+ * File description
+ * \author Thibaut DONTAIL
+ * \date 10 Feb. 2024
+ */
+
+/**
+ * \cond EXTERNAL_DOC
+ *
+ * \mainpage External documentation of module
+ *
+ * \section Description
+ * This module does what you want him to do....
+ *
+ * \section Features
+ * The main features of the module are described here.
+ *
+ * \endcond
+ */
+
+/* Prevent multiple inclusions */
+#ifndef INC_YACSGL_FONT_DIGIT_16X32_H_
+#ifndef INC_YACSGL_FONT_DIGIT_16X32_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/******************** INCLUDES ***********************************************/
+#include "YACSGL_font.h"
+
+/******************** CONSTANTS OF MODULE ************************************/
+
+/******************** MACROS DEFINITION **************************************/
+
+/******************** TYPE DEFINITION ****************************************/
+
+/******************** GLOBAL VARIABLES OF MODULE *****************************/
+extern const YACSGL_font_t YACSGL_font_digit_16x32;
+
+/******************** API FUNCTION PROTOTYPE *********************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* #ifndef INC_YACSGL_FONT_DIGIT_16X32_H_ */
+
+/**\} */
+/**\} */
+
+/* EOF */
